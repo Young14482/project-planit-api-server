5a62320824f274f01472fab03c4e8326de721d38
young <young14482@gmail.com>
2025-01-09 16:20:38 +0900

    [feat] 각 컨트롤러 사용할 메소드 골조 생성

--- src/main/java/com/metacoding/planitapiserver/category/CategoryController.java
+++ src/main/java/com/metacoding/planitapiserver/category/CategoryController.java
@@ -3,19 +3,35 @@ package com.metacoding.planitapiserver.category;
 import com.metacoding.planitapiserver._core.util.ApiUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class CategoryController {
     private final CategoryService categoryService;
+    // TODO 골조만 만든상태 컨트롤러 구현 해야함
 
-    @GetMapping("/api/category/{userid}")
-    public ResponseEntity<?> autoLogin(@RequestParam Integer userid) {
-
+    @GetMapping("/api/category")
+    public ResponseEntity<?> findCategory(@RequestParam Integer userid) {
+        categoryService.findCategory(userid);
         return ResponseEntity.ok(ApiUtil.success(null));
     }
 
+    @PostMapping("/api/category")
+    public ResponseEntity<?> saveCategory(@RequestParam Integer userid) {
+        categoryService.saveCategory(userid);
+        return ResponseEntity.ok(ApiUtil.success(null));
+    }
+
+    @PutMapping("/api/category")
+    public ResponseEntity<?> updateCategory(@RequestParam Integer categoryId, @RequestBody String name) {
+        categoryService.updateCategory(categoryId);
+        return ResponseEntity.ok(ApiUtil.success(null));
+    }
+
+    @DeleteMapping("/api/category")
+    public ResponseEntity<?> deleteCategory(@RequestParam Integer categoryId) {
+        categoryService.deleteCategory(categoryId);
+        return ResponseEntity.ok(ApiUtil.success(null));
+    }
 }
