1ffc1702e385677ea9e7f40d66bb4a212c9a1d7f
young <young14482@gmail.com>
2025-01-10 18:10:33 +0900

    [feat] db 구조 변경
    todo_tb isComplete 몇개 true로 변경

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -5,14 +5,14 @@ insert into user_tb(username, password, email, created_at, is_deleted) values('l
 INSERT INTO todo_tb (title, user_id, memo, due_date, created_at, repeat, is_completed, is_deleted)
 VALUES
     ('Task 1', 1, 'Memo for Task 1', DATEADD(DAY, -1, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매일', false, false),
-    ('Task 2', 1, 'Memo for Task 2', DATEADD(DAY, -2, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매주', false, false),
+    ('Task 2', 1, 'Memo for Task 2', DATEADD(DAY, -2, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매주', true, false),
     ('Task 3', 1, 'Memo for Task 3', DATEADD(DAY, -3, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매월', false, false),
-    ('Task 4', 1, 'Memo for Task 4', DATEADD(DAY, 0, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매년', false, false),
-    ('Task 5', 1, 'Memo for Task 5', DATEADD(DAY, 0, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '없음', false, false),
-    ('Task 6', 1, 'Memo for Task 6', DATEADD(DAY, 1, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매일', false, false),
+    ('Task 4', 1, 'Memo for Task 4', DATEADD(DAY, 0, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매년', true, false),
+    ('Task 5', 1, 'Memo for Task 5', DATEADD(DAY, 0, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '없음', true, false),
+    ('Task 6', 1, 'Memo for Task 6', DATEADD(DAY, 1, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매일', true, false),
     ('Task 7', 1, 'Memo for Task 7', DATEADD(DAY, 2, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매주', false, false),
-    ('Task 8', 1, 'Memo for Task 8', DATEADD(DAY, 3, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매월', false, false),
-    ('Task 9', 1, 'Memo for Task 9', DATEADD(DAY, 4, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매년', false, false),
+    ('Task 8', 1, 'Memo for Task 8', DATEADD(DAY, 3, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매월', true, false),
+    ('Task 9', 1, 'Memo for Task 9', DATEADD(DAY, 4, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '매년', true, false),
     ('Task 10', 1, 'Memo for Task 10', DATEADD(DAY, 30, CURRENT_TIMESTAMP), CURRENT_TIMESTAMP, '없음', false, false);
 
 INSERT INTO category_tb (name, user_id)
